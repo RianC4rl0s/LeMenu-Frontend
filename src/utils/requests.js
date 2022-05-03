eb2edf748a5b0ac7e5e9262af367709ba12fd44f
RianC4rl0s <riancarlos.sl@gmail.com>
2022-05-02 21:43:47 -0300

    refact back url

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,2 +1,2 @@
-// export const BASE_URL =  "https://le-menu-lab.herokuapp.com/";
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL =  "https://le-menu-lab.herokuapp.com/";
+// export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
