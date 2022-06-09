40f24c679982e7e8974f516911b09bad65780cb9
19RenanRibeiro <renanribeiro@bee.com.br>
2022-06-08 22:56:25 -0300

    feat: loading em tabela de mesas atendente

--- src/pages/TablesAttendant/index.js
+++ src/pages/TablesAttendant/index.js
@@ -2,6 +2,7 @@ import {
   Breadcrumb,
   // Button,
   Form,
+  Spinner,
   // OverlayTrigger,
   Stack,
   Table,
@@ -16,15 +17,22 @@ import api from "../../services/api";
 import { useEffect, useState } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 export default function TablesClerk() {
+  const [loading, setLoading] = useState(false);
   const [tableList, setTableList] = useState([]);
 
-  useEffect(() => {
-    api
+  const loadTables = async () => {
+    setLoading(true);
+    await api
       .get("/table/search/all")
       .then((response) => setTableList(response.data))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadTables();
   }, []);
 
   const [search, setSearch] = useState("");
@@ -61,41 +69,45 @@ export default function TablesClerk() {
                 </Form.Group>
                 <h3>Mesas</h3>
               </Styled.TitleContainer>
-
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Cod</th>
-                    <th>Cliente</th>
-                    <th>Total Pedido</th>
-                    <th>Disponibilidade</th>
-                    <th>Opções</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tableList
-                    .filter((tables) => tables.code.includes(search))
-                    .map((item) => (
-                      <tr key={item.id}>
-                        <td>{item.code}</td>
-                        <td>
-                          {item.client == null ? (
-                            <>Não tem cliente</>
-                          ) : (
-                            item.client.name
-                          )}
-                        </td>
-                        <td>0,00R$</td>
-                        <td>
-                          {item.isOpen ? (
-                            <label style={{ color: "green" }}>Aberta</label>
-                          ) : (
-                            <label style={{ color: "red" }}>Fechada</label>
-                          )}
-                        </td>
-                        <td>
-                          <Stack direction="horizontal" gap={1}>
-                            {/*
+              {loading ? (
+                <div style={{ alignItems: "center" }}>
+                  <Spinner animation="border" variant="success" />
+                </div>
+              ) : (
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Cod</th>
+                      <th>Cliente</th>
+                      <th>Total Pedido</th>
+                      <th>Disponibilidade</th>
+                      <th>Opções</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {tableList
+                      .filter((tables) => tables.code.includes(search))
+                      .map((item) => (
+                        <tr key={item.id}>
+                          <td>{item.code}</td>
+                          <td>
+                            {item.client == null ? (
+                              <>Não tem cliente</>
+                            ) : (
+                              item.client.name
+                            )}
+                          </td>
+                          <td>0,00R$</td>
+                          <td>
+                            {item.isOpen ? (
+                              <label style={{ color: "green" }}>Aberta</label>
+                            ) : (
+                              <label style={{ color: "red" }}>Fechada</label>
+                            )}
+                          </td>
+                          <td>
+                            <Stack direction="horizontal" gap={1}>
+                              {/*
                             <OverlayTrigger
                               placement="bottom"
                               overlay={
@@ -108,12 +120,13 @@ export default function TablesClerk() {
                                 <FaSearch color="white" />
                               </Button>
                             </OverlayTrigger>*/}
-                          </Stack>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </Table>
+                            </Stack>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </Table>
+              )}
             </Styled.BorderContainer>
           </Styled.Container>
         </div>
