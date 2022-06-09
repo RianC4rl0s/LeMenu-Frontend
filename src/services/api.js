75cb669f1eb1a77dc9058872bcc72b3beca8b800
RianC4rl0s <riancarlos.sl@gmail.com>
2022-06-09 09:09:17 -0300

    refact: api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { BASE_URL }  from "../utils/requests"
 // import { parseCookies } from 'nookies';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  //baseURL: 'http://localhost:8080'
+  baseURL: BASE_URL
 })
 
 api.interceptors.request.use((config) => {
